40e8f11a51ecd23f523b3b7120f9bcd01e25dfd9
Denys Bzenko <dbzenko@kse.org.ua>
2023-11-01 00:42:35 +0200

    Animation

--- program/script/index.js
+++ program/script/index.js
@@ -1,100 +1,120 @@
 $(document).ready(function() {
+    // FAQ Toggle
     $('.faq-list li').click(function() {
         $(this).find('.faq-answer').slideToggle();
         $(this).find('span:last-child').toggleClass('open');
     });
 
+    // Open Booking Modal
     $('.book-now-main, .book-now-top').click(function() {
         $('#bookingModal').fadeIn();
     });
 
-    $('.close').click(function() {
+    // Close Booking Modal
+    $('#bookingModal .close, #bookingModal').click(function(e) {
+        if (e.target !== this) return;
         $('#bookingModal').fadeOut();
     });
+
+    // Submit Button
+    $("#submitButton").click(function() {
+        if (validateFormData()) {
+            var serviceName = $("#serviceSelection").val();
+            var name = $("#nameInput").val();
+            var surname = $("#surnameInput").val();
+            var email = $("#emailInput").val();
+
+            console.log("Selected Service:", serviceName);
+            console.log("Name:", name);
+            console.log("Surname:", surname);
+            console.log("Email:", email);
+            
+            submitForm();  // Call the submitForm function after validation
+        }
+    });
 });
-
-document.getElementById("submitButton").addEventListener("click", function() {
-    if (validateFormData()) {
-        var serviceName = document.getElementById("serviceSelection").value;
-        var name = document.getElementById("nameInput").value;
-        var surname = document.getElementById("surnameInput").value;
-        var email = document.getElementById("emailInput").value;
-
-        console.log("Selected Service:", serviceName);
-        console.log("Name:", name);
-        console.log("Surname:", surname);
-        console.log("Email:", email);
-    }
-});
-
-var bookingModal = document.getElementById("bookingModal");
-var closeModal = document.querySelector(".close");
-
-closeModal.addEventListener("click", function() {
-    bookingModal.style.display = "none";
-});
-
-window.onclick = function(event) {
-    if (event.target == bookingModal) {
-        bookingModal.style.display = "none";
-    }
-}
 
 function validateFormData() {
     let isValid = true;
 
-    const name = document.getElementById("nameInput");
-    const surname = document.getElementById("surnameInput");
-    const email = document.getElementById("emailInput");
+    const name = $("#nameInput");
+    const surname = $("#surnameInput");
+    const email = $("#emailInput");
     
     const hasNumbers = /\d/;
     
-    if(hasNumbers.test(name.value)) {
-        name.classList.add("invalid");
-        document.getElementById("nameTooltip").style.display = "block";
+    if(hasNumbers.test(name.val())) {
+        name.addClass("invalid");
+        $("#nameTooltip").show();
         isValid = false;
     } else {
-        name.classList.remove("invalid");
-        document.getElementById("nameTooltip").style.display = "none";
+        name.removeClass("invalid");
+        $("#nameTooltip").hide();
     }
     
-    if(hasNumbers.test(surname.value)) {
-        surname.classList.add("invalid");
-        document.getElementById("surnameTooltip").style.display = "block";
+    if(hasNumbers.test(surname.val())) {
+        surname.addClass("invalid");
+        $("#surnameTooltip").show();
         isValid = false;
     } else {
-        surname.classList.remove("invalid");
-        document.getElementById("surnameTooltip").style.display = "none";
+        surname.removeClass("invalid");
+        $("#surnameTooltip").hide();
     }
     
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    if(!emailPattern.test(email.value)) {
-        email.classList.add("invalid");
-        document.getElementById("emailTooltip").style.display = "block";
+    if(!emailPattern.test(email.val())) {
+        email.addClass("invalid");
+        $("#emailTooltip").show();
         isValid = false;
     } else {
-        email.classList.remove("invalid");
-        document.getElementById("emailTooltip").style.display = "none";
+        email.removeClass("invalid");
+        $("#emailTooltip").hide();
     }
 
     if (!isValid) {
-        document.getElementById("submitTooltip").style.display = "block";
+        $("#submitTooltip").show();
     } else {
-        document.getElementById("submitTooltip").style.display = "none";
+        $("#submitTooltip").hide();
     }
 
     return isValid;
 }
-function submitForm() {
-    if (validateFormData()) {
-        // Here you can add the logic to send the data to your server...
-        
-        // Display the success modal
-        document.getElementById("successModal").style.display = "block";
 
-        // Hide the success modal after 3 seconds
-        setTimeout(() => {
-            document.getElementById("successModal").style.display = "none";
-        }, 3000);
-    }
+function submitForm() {
+    // Here you can add the logic to send the data to your server...
+    
+    // Close the booking modal
+    $("#bookingModal").fadeOut();
+    
+    // Display the success modal
+    $("#successModal").show();
+
+    // Hide the success modal after 3 seconds
+    setTimeout(() => {
+        $("#successModal").hide();
+    }, 3000);
 }
+document.getElementById('gallery').addEventListener('click', function() {
+    // Приховуємо основний контент
+    document.getElementById('mainContent').style.display = 'none';
+  
+    // Завантажуємо фотографію собаки
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        let dogImage = document.createElement('img');
+        dogImage.src = data.message;
+        document.getElementById('dogImagesContainer').appendChild(dogImage);
+  
+        // Відображаємо галерею
+        document.getElementById('dogGallery').style.display = 'block';
+      })
+      .catch(error => console.error('Error fetching dog image:', error));
+  });
+  
+  // Кнопка "Назад"
+  document.getElementById('backToMain').addEventListener('click', function() {
+    document.getElementById('dogGallery').style.display = 'none';
+    document.getElementById('mainContent').style.display = 'block';
+  });
+  
