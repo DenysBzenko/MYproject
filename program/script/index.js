528f1c3040639a10a3710c64c4e69b1dbae92543
Denys Bzenko <dbzenko@kse.org.ua>
2023-11-01 00:21:10 +0200

    Great modal

--- program/script/index.js
+++ program/script/index.js
@@ -3,28 +3,28 @@ $(document).ready(function() {
         $(this).find('.faq-answer').slideToggle();
         $(this).find('span:last-child').toggleClass('open');
     });
-});
 
-$(document).ready(function() {
     $('.book-now-main, .book-now-top').click(function() {
         $('#bookingModal').fadeIn();
     });
 
-    $('.close-btn').click(function() {
+    $('.close').click(function() {
         $('#bookingModal').fadeOut();
     });
 });
 
-document.getElementById("submitBtn").addEventListener("click", function() {
-    var serviceName = document.getElementById("serviceSelection").value;
-    var name = document.getElementById("nameInput").value;
-    var surname = document.getElementById("surnameInput").value;
-    var email = document.getElementById("emailInput").value;
+document.getElementById("submitButton").addEventListener("click", function() {
+    if (validateFormData()) {
+        var serviceName = document.getElementById("serviceSelection").value;
+        var name = document.getElementById("nameInput").value;
+        var surname = document.getElementById("surnameInput").value;
+        var email = document.getElementById("emailInput").value;
 
-    console.log("Selected Service:", serviceName);
-    console.log("Name:", name);
-    console.log("Surname:", surname);
-    console.log("Email:", email);
+        console.log("Selected Service:", serviceName);
+        console.log("Name:", name);
+        console.log("Surname:", surname);
+        console.log("Email:", email);
+    }
 });
 
 var bookingModal = document.getElementById("bookingModal");
@@ -39,6 +39,7 @@ window.onclick = function(event) {
         bookingModal.style.display = "none";
     }
 }
+
 function validateFormData() {
     let isValid = true;
 
@@ -47,34 +48,53 @@ function validateFormData() {
     const email = document.getElementById("emailInput");
     
     const hasNumbers = /\d/;
-    if(hasNumbers.test(name.value) || hasNumbers.test(surname.value)) {
+    
+    if(hasNumbers.test(name.value)) {
         name.classList.add("invalid");
-        surname.classList.add("invalid");
+        document.getElementById("nameTooltip").style.display = "block";
         isValid = false;
     } else {
         name.classList.remove("invalid");
-        name.classList.add("valid");
+        document.getElementById("nameTooltip").style.display = "none";
+    }
+    
+    if(hasNumbers.test(surname.value)) {
+        surname.classList.add("invalid");
+        document.getElementById("surnameTooltip").style.display = "block";
+        isValid = false;
+    } else {
         surname.classList.remove("invalid");
-        surname.classList.add("valid");
+        document.getElementById("surnameTooltip").style.display = "none";
     }
     
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if(!emailPattern.test(email.value)) {
         email.classList.add("invalid");
+        document.getElementById("emailTooltip").style.display = "block";
         isValid = false;
     } else {
         email.classList.remove("invalid");
-        email.classList.add("valid");
+        document.getElementById("emailTooltip").style.display = "none";
     }
 
-    document.getElementById("submitButton").disabled = !isValid;
+    if (!isValid) {
+        document.getElementById("submitTooltip").style.display = "block";
+    } else {
+        document.getElementById("submitTooltip").style.display = "none";
+    }
 
     return isValid;
 }
-
 function submitForm() {
     if (validateFormData()) {
-        // Тут можна додати додатковий код, який ви хочете виконати після успішної валідації
-        console.log("Форма відправлена!");
+        // Here you can add the logic to send the data to your server...
+        
+        // Display the success modal
+        document.getElementById("successModal").style.display = "block";
+
+        // Hide the success modal after 3 seconds
+        setTimeout(() => {
+            document.getElementById("successModal").style.display = "none";
+        }, 3000);
     }
 }
