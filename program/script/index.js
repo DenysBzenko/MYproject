13bb1b4704646d3a1537a276a3607b7e81147950
Denys Bzenko <dbzenko@kse.org.ua>
2023-11-01 01:48:22 +0200

    Add contact

--- program/script/index.js
+++ program/script/index.js
@@ -1,120 +1,117 @@
+function validateEmail(email) {
+    var re = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function validateName(name) {
+    return name.length >= 2;
+}
+
+
+function openModal() {
+    $('#bookingModal').show();
+}
+
+function closeModal() {
+    $('#bookingModal').hide();
+    $('#successModal').hide();
+}
+
+
 $(document).ready(function() {
-    // FAQ Toggle
-    $('.faq-list li').click(function() {
-        $(this).find('.faq-answer').slideToggle();
-        $(this).find('span:last-child').toggleClass('open');
+
+    
+    $('.close').on('click', function() {
+        closeModal();
     });
 
-    // Open Booking Modal
-    $('.book-now-main, .book-now-top').click(function() {
-        $('#bookingModal').fadeIn();
+    $('.book-now-top, .book-now-main').on('click', function() {
+        openModal();
     });
 
-    // Close Booking Modal
-    $('#bookingModal .close, #bookingModal').click(function(e) {
-        if (e.target !== this) return;
-        $('#bookingModal').fadeOut();
-    });
+    
+    $('#submitButton').on('click', function() {
+        let nameValid = validateName($('#nameInput').val());
+        let surnameValid = validateName($('#surnameInput').val());
+        let emailValid = validateEmail($('#emailInput').val());
 
-    // Submit Button
-    $("#submitButton").click(function() {
-        if (validateFormData()) {
-            var serviceName = $("#serviceSelection").val();
-            var name = $("#nameInput").val();
-            var surname = $("#surnameInput").val();
-            var email = $("#emailInput").val();
+        $('#nameTooltip').toggle(!nameValid);
+        $('#surnameTooltip').toggle(!surnameValid);
+        $('#emailTooltip').toggle(!emailValid);
+        $('#submitTooltip').toggle(!(nameValid && surnameValid && emailValid));
 
-            console.log("Selected Service:", serviceName);
-            console.log("Name:", name);
-            console.log("Surname:", surname);
-            console.log("Email:", email);
+        if (nameValid && surnameValid && emailValid) {
+            closeModal();
+            $('#successModal').show();
+
             
-            submitForm();  // Call the submitForm function after validation
+            setTimeout(function() {
+                $('#successModal').hide();
+            }, 3000);
         }
+    });
+
+    
+    $("ul.menu li a:contains('Gallery')").on('click', function(e) {
+        e.preventDefault();
+        $('.container, nav, header, .spa-treatment, .professional, .expect-care, .review-container, .faq-section').hide();
+        $('#dogGallery').show();
+
+       
+        loadNewDog();
+    });
+
+    $('#nextDog').on('click', function() {
+        loadNewDog();
+    });
+
+    $('#prevDog').on('click', function() {
+        let images = $('#dogImagesContainer img');
+        if (images.length > 1) {
+            images.last().remove();
+        }
+    });
+
+    
+    $('#backToMain').on('click', function() {
+        $('#dogGallery').hide();
+        $('.container, nav, header, .spa-treatment, .professional, .expect-care, .review-container, .faq-section').show();
     });
 });
 
-function validateFormData() {
-    let isValid = true;
-
-    const name = $("#nameInput");
-    const surname = $("#surnameInput");
-    const email = $("#emailInput");
-    
-    const hasNumbers = /\d/;
-    
-    if(hasNumbers.test(name.val())) {
-        name.addClass("invalid");
-        $("#nameTooltip").show();
-        isValid = false;
-    } else {
-        name.removeClass("invalid");
-        $("#nameTooltip").hide();
-    }
-    
-    if(hasNumbers.test(surname.val())) {
-        surname.addClass("invalid");
-        $("#surnameTooltip").show();
-        isValid = false;
-    } else {
-        surname.removeClass("invalid");
-        $("#surnameTooltip").hide();
-    }
-    
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    if(!emailPattern.test(email.val())) {
-        email.addClass("invalid");
-        $("#emailTooltip").show();
-        isValid = false;
-    } else {
-        email.removeClass("invalid");
-        $("#emailTooltip").hide();
-    }
-
-    if (!isValid) {
-        $("#submitTooltip").show();
-    } else {
-        $("#submitTooltip").hide();
-    }
-
-    return isValid;
-}
-
-function submitForm() {
-    // Here you can add the logic to send the data to your server...
-    
-    // Close the booking modal
-    $("#bookingModal").fadeOut();
-    
-    // Display the success modal
-    $("#successModal").show();
-
-    // Hide the success modal after 3 seconds
-    setTimeout(() => {
-        $("#successModal").hide();
-    }, 3000);
-}
-document.getElementById('gallery').addEventListener('click', function() {
-    // Приховуємо основний контент
-    document.getElementById('mainContent').style.display = 'none';
-  
-    // Завантажуємо фотографію собаки
+function loadNewDog() {
     fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(data => {
-        let dogImage = document.createElement('img');
-        dogImage.src = data.message;
-        document.getElementById('dogImagesContainer').appendChild(dogImage);
-  
-        // Відображаємо галерею
-        document.getElementById('dogGallery').style.display = 'block';
-      })
-      .catch(error => console.error('Error fetching dog image:', error));
-  });
-  
-  // Кнопка "Назад"
-  document.getElementById('backToMain').addEventListener('click', function() {
-    document.getElementById('dogGallery').style.display = 'none';
-    document.getElementById('mainContent').style.display = 'block';
-  });
-  
+        .then(response => response.json())
+        .then(data => {
+            let img = $('<img>', {src: data.message, alt: 'Happy Dog', class: 'dog-image-gallery'});
+            $('#dogImagesContainer').append(img);
+
+           
+            $('#dogImagesContainer').scrollLeft($('#dogImagesContainer')[0].scrollWidth);
+        })
+        .catch(error => {
+            console.log('Error fetching dog image:', error);
+        });
+}
+   
+    $('.faq-list li span:nth-child(2)').on('click', function() {
+        $(this).next('.faq-answer').slideToggle();
+        if ($(this).text() === '+') {
+            $(this).text('-');
+        } else {
+            $(this).text('+');
+        }
+    });
+    $("ul.menu li a:contains('Contact')").on('click', function(e) {
+
+        e.preventDefault();
+        $('.container, nav, header, .spa-treatment, .professional, .expect-care, .review-container, .faq-section, #dogGallery').hide();
+        $('#contactSection').show();
+    });
+    
+    $('#backFromContact').on('click', function() {
+        $('#contactSection').hide();
+        $('.container, nav, header, .spa-treatment, .professional, .expect-care, .review-container, .faq-section').show();
+    });
+    
